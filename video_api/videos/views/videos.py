c9f90193d51f1aa765e22e55ca433c8145d5da98
swastik-singh-0301 <77483481+swastik-singh-0301@users.noreply.github.com>
2024-11-20 23:51:18 +0530

    feat: API to merge already uploaded videos

--- video_api/videos/views/videos.py
+++ video_api/videos/views/videos.py
@@ -4,7 +4,7 @@ from videos.models import Video
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
 from django.conf import settings
 import ffmpeg
@@ -102,9 +102,16 @@ class VideoViewSet(viewsets.ModelViewSet):
         trimmed_video = trimmed_video.set_audio(trimmed_audio)
         return trimmed_video
 
-    def write_video_to_file(self, trimmed_video, output_path):
+    def write_video_to_file(self, video, output_path):
         """Write the trimmed video to the specified output file."""
-        trimmed_video.write_videofile(output_path, codec="libx264", audio_codec="aac")
+        video.write_videofile(output_path, codec="libx264", audio_codec="aac")
+
+    def get_output_path(self, function, input_filepath=None):
+        filename = os.path.basename(input_filepath)
+        if input_filepath:
+            return os.path.join(settings.MEDIA_ROOT, function, self.generate_unique_filename(f"{function}_{filename}"))
+        return os.path.join(settings.MEDIA_ROOT, function, self.generate_unique_filename(f"{function}"))
+
 
     def trim_video(self, video_path, start_time, end_time):
         """Main function to trim the video."""
@@ -116,8 +123,7 @@ class VideoViewSet(viewsets.ModelViewSet):
         target_width, target_height = self.calculate_trimmed_video_size(video, target_height)
         trimmed_video = self.resize_video(trimmed_video, target_size=(target_height, target_width))
 
-        output_filename = self.generate_unique_filename(video_path)
-        output_path = os.path.join(settings.MEDIA_ROOT, 'trimmed_videos', output_filename)
+        output_path = self.get_output_path("trimmed_videos", video_path)
 
         self.ensure_directory_exists(os.path.dirname(output_path))
         self.write_video_to_file(trimmed_video, output_path)
@@ -125,3 +131,67 @@ class VideoViewSet(viewsets.ModelViewSet):
         video.close()
 
         return trimmed_video, output_path
+
+    @action(detail=False, methods=['post'])
+    def merge(self, request):
+        """Merge multiple video clips into a single video."""
+        video_ids = request.data.get('video_ids', [])
+
+        # Validate the input video_ids
+        if not self.validate_merge_videos(video_ids):
+            return Response({"error": "Please provide at least two videos to merge."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Fetch videos from the database
+        videos = Video.objects.filter(id__in=video_ids)
+        if len(videos) != len(video_ids):
+            return Response({"error": "One or more videos not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        # Extract the file paths of the videos
+        video_paths = [video.file.path for video in videos]
+
+        # Merge the videos
+        merged_video, output_path = self.merge_videos(video_paths)
+
+        # Create a new Video object for the merged video
+        new_video = Video.objects.create(
+            title="Merged Video",
+            file=output_path,
+        )
+
+        # Return the merged video URL
+        serializer = self.serializer_class(new_video, context={'request': request})
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    def merge_videos(self, video_paths):
+        """Merge a list of video clips into a single video file."""
+        clips = []
+        # Load video clips into memory
+        for path in video_paths:
+            video_clip = VideoFileClip(path)
+            clips.append(video_clip)
+
+        # Concatenate video clips
+        final_clip = concatenate_videoclips(clips, method="compose")
+
+        # Generate a unique filename
+        output_filename = self.get_output_path(function="merged_videos")
+
+        # Ensure output directory exists
+        self.ensure_directory_exists(output_filename)
+
+        # Write the final video to file
+        final_clip.write_videofile(output_filename, codec="libx264", audio_codec="aac")
+
+        # Close the clips to release resources
+        for clip in clips:
+            clip.close()
+
+        return final_clip,  output_filename
+
+    def validate_merge_videos(self, video_ids):
+        if len(video_ids) < 2:
+            return False
